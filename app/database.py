156a2e1a553cd746f1b451f0bccbf270d9bfc106
valeriiashmyhlo <valeriia.ibadullaieva@gmail.com>
2024-02-02 20:44:07 +0000

    revert to postgres

--- app/database.py
+++ app/database.py
@@ -2,29 +2,30 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 
-db_name = "database"
-db_user = "user"
-db_pass = "pass"
-db_host = "localhost"
-db_port = "5432"
+# db_name = "database"
+# db_user = "user"
+# db_pass = "pass"
+# db_host = "localhost"
+# db_port = "5432"
 
-db_sqlite_string = "sqlite:///db.sqlite".format(
-    db_user, db_pass, db_host, db_port, db_name
-)
-db_string = "postgresql://{}:{}@{}:{}/{}".format(
-    db_user, db_pass, db_host, db_port, db_name
-)
+# db_sqlite_string = "sqlite:///db.sqlite".format(
+#     db_user, db_pass, db_host, db_port, db_name
+# )
+# db_string = "postgresql://{}:{}@{}:{}/{}".format(
+#     db_user, db_pass, db_host, db_port, db_name
+# )
 
-engine = create_engine(db_sqlite_string)
+engine = create_engine(os.environ["DATABASE_URL"])
 
 
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(db_connection, connection_record):
-    cursor = db_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
+# @event.listens_for(Engine, "connect")
+# def set_sqlite_pragma(db_connection, connection_record):
+#     cursor = db_connection.cursor()
+#     cursor.execute("PRAGMA foreign_keys=ON")
+#     cursor.close()
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
